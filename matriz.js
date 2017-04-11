dc7c806a1c451cc0625c96d111765be654f49718
Alejo Moreno <tecksonic@gmail.com>
2017-04-11 13:14:58 -0500

    Initial commit

---
+++ matriz.js
@@ -0,0 +1,53 @@
+'use strict';
+
+var matriz = {
+
+    'Amoroso': {
+        id: 1,
+        estrofas: [
+            1,
+            2,
+            3,
+            4,
+            5,
+            6,
+            9,
+            10
+        ]
+    },
+    'Cuernos': {
+        id: 2,
+        estrofas: [11, 12]
+    },
+    'Piesteando': {
+        id: 3,
+        estrofas: [7, 8]
+    },
+    'Arrepentido': {
+        id: 4,
+        estrofas: [14]
+    },
+    'Al tiro': {
+        id: 5,
+        estrofas: [7, 8]
+    },
+    'Tristeando': {
+        id: 6,
+        estrofas: [1, 2, 3, 5, 6]
+    },
+    'Echando los perros': {
+        id: 7,
+        estrofas: [
+            3,
+            4,
+            7,
+            8,
+            9,
+            10
+        ]
+    },
+    'Dolido': {
+        id: 7,
+        estrofas: [1, 5, 6, 10]
+    }
+}
